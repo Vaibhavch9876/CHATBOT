d5bd4f51b4c0fe7973e5779a60bbab331ed87b8e
Abhishek <notabhishektiwari@gmail.com>
2020-11-01 05:14:10 +0530

    Resolved Bugs

--- Chatbot/problems/urls.py
+++ Chatbot/problems/urls.py
@@ -4,6 +4,7 @@ from .views import HomePageView, SearchResultsView
 
 
 urlpatterns = [
-    path('search/', SearchResultsView.as_view(), name='search_results'),
+    # path('search/', SearchResultsView.as_view(), name='search_results'),
+    path('', SearchResultsView.as_view(), name='search_results'),
     path('', HomePageView.as_view(), name='home'),
 ]
