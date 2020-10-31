51467affc5c74d29a73950f349d48e1419018d0e
Abhishek <notabhishektiwari@gmail.com>
2020-10-31 17:24:41 +0530

    Changed search_results.html

--- Chatbot/problems/admin.py
+++ Chatbot/problems/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 from .models import Problem
 
 class ProblemAdmin(admin.ModelAdmin):
-    list_display = ("p_id", "p_statement",)
+    # p_id = models.AutoField(primary_key=True)
+    # p_statement = models.TextField(max_length=1000, blank=False)
+    # p_tags = models.CharField(max_length=1000, blank=False)
+    # p_rating = models.IntegerField()
+    # p_A = models.CharField(blank=False, max_length=100)
+    # p_B = models.CharField(blank=False, max_length=100)
+    # p_C = models.CharField(max_length=100, blank=True)
+    # p_D = models.CharField(max_length=100, blank=True)
+    # p_E = models.CharField(max_length=100, blank=True)
+    # p_correct = models.CharField(blank=False, max_length=100)
+    list_display = ("p_id", "p_statement", "p_tags" , "p_rating" , "p_A" , "p_B" , "p_C" , "p_D" , "p_E" , "p_correct")
 
 admin.site.register(Problem, ProblemAdmin)
--- Chatbot/problems/models.py
+++ Chatbot/problems/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Problem(models.Model):
     p_id = models.AutoField(primary_key=True)
-    p_statement = models.CharField(max_length=1000, blank=False)
+    p_statement = models.TextField(max_length=1000, blank=False)
     p_tags = models.CharField(max_length=1000, blank=False)
     p_rating = models.IntegerField()
     p_A = models.CharField(blank=False, max_length=100)
